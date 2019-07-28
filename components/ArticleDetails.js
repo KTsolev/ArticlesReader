33b54776e7dbfbab168dfd3fe533e39a70af4d42
Konstantin Tsolev <ktsolev@gmail.com>
2019-07-28 16:12:33 +0300

    added spinner before loading articles in webview

--- components/ArticleDetails.js
+++ components/ArticleDetails.js
@@ -1,6 +1,6 @@
 
 import React from "react";
-import { View, Text, Button } from "react-native";
+import { View, Text, Button, ActivityIndicator } from "react-native";
 import { headerConfiguration } from './Header';
 import { WebView } from 'react-native-webview';
 
@@ -8,14 +8,25 @@ class ArticleDetails extends React.Component {
     static navigationOptions = headerConfiguration;
     render() {
       const { navigation } = this.props;
-      const itemId = navigation.getParam('itemId', 'NO-ID');
-      const url = navigation.getParam('url', 'some default value');
+      const url = navigation.getParam('url', null);
       return (
         <WebView
+          styles={{position: 'relative'}}
           source={{ uri: url }}
-          startInLoadingState={false}
+          startInLoadingState={true}
           javaScriptEnabled={true}
-          thirdPartyCookiesEnabled={true}
+          renderLoading={() => <View style={{
+            justifyContent: 'center',
+            alingItems: 'center',
+            position: 'absolute',
+            zIndex:1,
+            top: 0, 
+            bottom: 0, 
+            left: 0, 
+            right: 0, 
+            backgroundColor:'rgba(0, 179, 149, 0.35)'}}>
+            <ActivityIndicator size="large" color="#fff" />
+          </View>}
           scalesPageToFit
         />
       );
